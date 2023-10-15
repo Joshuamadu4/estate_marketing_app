e296b338fbc883f50fd024a40532a53cf1110fc5
joshuamadu4 <joshuamadu4@gmail.com>
2023-10-15 13:09:07 +0100

    create and add OAuth components

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema({
         type: String, 
         required: true,
     },
+    avatar:{
+        type: String, 
+        default: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi0.wp.com%2Fvssmn.org%2Fwp-content%2Fuploads%2F2018%2F12%2F146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png%3Ffit%3D860%252C681%26ssl%3D1&f=1&nofb=1&ipt=79f2bfc98d16685d6c45d25d367f8886dccf327a028f139e8b044046bc30adbd&ipo=images"
+        
+    }
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
