072437ae435400c2f0ad21ab2b0141c5f585f7a5
joshuamadu4 <joshuamadu4@gmail.com>
2023-10-17 16:33:31 +0100

    create user update API

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -71,7 +71,7 @@ export default function Profile() {
           
         <input type="text" placeholder='username' id="username" className="border p-3 rounded-lg"/>
         <input type="email" placeholder='email' id="email" className="border p-3 rounded-lg"/>
-        <input type="text" placeholder='password' id="password" className="border p-3 rounded-lg"/>
+        <input type="password" placeholder='password' id="password" className="border p-3 rounded-lg"/>
         <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-70 
         disabled: opacity-80">Update</button>
       </form>
